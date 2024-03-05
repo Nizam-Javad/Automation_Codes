88069271073583795bc29eaf40064fb710cbd3e3
Nizam-Javad <nizameee22@gmail.com>
2024-03-05 11:45:41 +0530

    Mar 5 commit1

---
+++ src/main/java/com/test_automation1/utilities/TestUtils.java
@@ -0,0 +1,11 @@
+package com.test_automation1.utilities;
+
+import com.test_automation1.pages.BaseTest;
+
+public class TestUtils extends BaseTest {
+
+    public void switchToFrame() {
+        driver.switchTo().frame("");
+    }
+
+}
